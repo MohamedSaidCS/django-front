f81894f736a68fedcd5fb729054981b9a7854fa7
HassanTawfic <hatawfic@gmail.com>
2022-06-16 15:29:11 +0200

    home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import background from '../assets/images/fotis-fotopoulos-LJ9KY8pIH3E-unsplash.jpg'
 
 function Home() {
     return (
         <div>
-            home page
+            <div style={{backgroundImage : `url(${background})`, backgroundSize:'cover', height:'100vh'}}>
+                <p className={'display-1 p-5 text-light mt-5 ms-3'}> Hello, Guest!</p>
+                <div className={'d-flex justify-content-center align-items-center'}>
+                    <button className={'btn btn-info btn-lg text-white p-2 m-2'}>Login</button>
+                    <button className={'btn btn-info btn-lg text-white p-2 m-2'}>Sign Up</button>
+                </div>
+            </div>
         </div>
     );
 }
