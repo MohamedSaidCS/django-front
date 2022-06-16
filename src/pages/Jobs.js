932b122b76944ebb3530e77bc1ed2ca366387291
yhyamohamed <yhyashehata1@gmail.com>
2022-06-16 16:25:01 +0200

    list all jobs component

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,11 +1,26 @@
-import React from 'react';
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 function Jobs() {
-    return (
-        <div>
-            jobs
-        </div>
-    );
+  const [jobsList, SetjobsList] = useState(null);
+  const getAllJobs = async () => {
+    const response = await axios.get("http://127.0.0.1:8000/api/jobs/");
+    const data = await response.data;
+
+    SetjobsList(data);
+  };
+  useEffect(() => {
+    getAllJobs();
+  }, []);
+
+  return (
+    <div>
+      jobs
+      {jobsList &&
+        jobsList.map((job) => {
+          console.log(job);
+        })}
+    </div>
+  );
 }
 
 export default Jobs;
