0d2704dd8432c8a06685c8d8809d0161fb8728a9
HassanTawfic <hatawfic@gmail.com>
2022-06-18 00:17:00 +0200

    styling profile

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -11,7 +11,7 @@ function Profile() {
     const [loading, setLoading] = useState(true);
 
     const fetchUser = async () => {
-        if(id === user.id || !id) {
+        if (id === user.id || !id) {
             setProfileUser(user);
         } else {
             const response = await axios.get(`http://127.0.0.1:8000/api/profiles/${id}`, {
@@ -31,17 +31,17 @@ function Profile() {
         console.log(profileUser);
     }, []);
 
-    if(loading) {
+    if (loading) {
         return <div>Loading...</div>
     }
 
     return (
         <div style={{
-                backgroundImage: `url(${background})`,
-                backgroundSize: 'cover',
-                height: '100vh',
-                paddingTop: "60px"
-            }}>
+            backgroundImage: `url(${background})`,
+            backgroundSize: 'cover',
+            height: '100vh',
+            paddingTop: "60px"
+        }}>
             <div className={'container'}>
                 <div className={'d-flex justify-content-center'}>
                     <div className="card text-center col-6">
@@ -51,10 +51,10 @@ function Profile() {
                         <div className="card-body">
                             <h5 className="card-title fs-4">{profileUser.username.toUpperCase()}</h5>
                             <p className="card-text fw-bold fs-5">
-                            <span>E-Mail: {profileUser.email}</span><br/>
-                            <span>Gender: {profileUser.gender}</span><br/>
-                            <span>Technologies: {profileUser.tags.map((tag)=>tag.name).join(', ')}</span><br/>
-                            <span>Address: {profileUser.address}</span>
+                                <span>E-Mail: {profileUser.email}</span><br/>
+                                <span>Gender: {profileUser.gender}</span><br/>
+                                <span>Technologies: {profileUser.tags.map((tag) => tag.name).join(', ')}</span><br/>
+                                <span>Address: {profileUser.address}</span>
                             </p>
                             <div>
                                 <button className={'btn btn-success btn-lg'}>Edit Profile</button>
